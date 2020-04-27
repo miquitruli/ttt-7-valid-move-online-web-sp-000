c6341f846499253b3fb06d512f4cdea8175a2af2
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-04-27 23:45:43 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -16,7 +16,7 @@ def position_taken?(board, index)
   if (board[index] == "X" || board[index] == "O")
     return true
   elsif board[index] == " "
-    return true
+    return false
   elsif board[index] == ""
     return false
   elsif board[index] == nil
