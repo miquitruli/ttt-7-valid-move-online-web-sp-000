cc2835daeb7fdaf799a0a37cf2b907eae7bc0cfb
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-04-27 23:57:28 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,19 +5,26 @@
 
 
 def valid_move?(board, index)
-  if position_taken? == true
-    return nil
-  end
-end
 
-def position_taken?(array, ind)
-  if (array[ind] == "X" || array[index] == "O")
-    return true
-  elsif array[ind] == " "
-    return false
-  elsif array[ind] == ""
-    return false
-  elsif array[ind] == nil
-    return false
+  def position_taken?(array, ind)
+    if (array[ind] == "X" || array[index] == "O")
+      return true
+    elsif array[ind] == " "
+      return false
+    elsif array[ind] == ""
+      return false
+    elsif array[ind] == nil
+      return false
+    end
   end
+
+    def number_valid?(num)
+      if num.between?(0, 8) == true
+        return true
+      else
+        return false
+      end
+    end
+
+    
 end
