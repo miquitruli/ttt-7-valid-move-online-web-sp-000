6ec353205ebfef2a8c99e27507e54d620b451e4e
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-04-27 23:36:36 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -13,12 +13,3 @@ def position_taken?(board, index)
     return false
   end
 end
-
-
-def valid_move?
- if position_taken? == true
-   return nil
- elsif position_taken? == false
-   return true
- end
-end
